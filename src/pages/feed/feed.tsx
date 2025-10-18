7bd8ddecc0e438c0fda213ba1d203c783aac90a5
Matheus <matheusfelipem336@gmail.com>
2025-10-18 09:22:06 -0300

    Adição de Dropdown e Avatar de usuário

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -9,11 +9,26 @@ import {Input} from "@/components/ui/input.tsx";
 import {Link} from "@tanstack/react-router";
 import logo from "@/assets/shark-in.png";
 import {IconButton} from "@mui/material";
-import Chat from '@mui/icons-material/ChatOutlined';
 import {Textarea} from "@/components/ui/textarea"
 import {Button} from "@/components/ui/button.tsx";
-import Notifications from '@mui/icons-material/NotificationsOutlined';
 import Account from '@mui/icons-material/AccountCircleOutlined';
+import { useState } from "react"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 interface Post {
     id: string
@@ -156,6 +171,109 @@ const PostItem = ({post}: { post: Post }) => {
     )
 }
 
+const DropdownMenuDialog = () => {
+    const [showNewDialog, setShowNewDialog] = useState(false)
+    const [showShareDialog, setShowShareDialog] = useState(false)
+    const [showMessageDialog, setShowMessageDialog] = useState(false)
+
+    return (
+        <>
+            <DropdownMenu modal={false}>
+                <DropdownMenuTrigger asChild>
+                    <Avatar>
+                        <AvatarImage src={'https://i.pravatar.cc/150?img=12'} alt={'Matheus Felipe'}/>
+                        <AvatarFallback>{'Matheus Felipe'}</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-40" align="end">
+                    <DropdownMenuLabel>Matheus Aguiar</DropdownMenuLabel>
+                    <DropdownMenuGroup>
+                        <DropdownMenuItem onSelect={() => setShowMessageDialog(true)}>
+                            Mensagens
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={() => setShowShareDialog(true)}>
+                            Notificações
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={() => setShowNewDialog(true)}>
+                            Sair
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            <Dialog open={showNewDialog} onOpenChange={setShowNewDialog}>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Deseja sair?</DialogTitle>
+                    </DialogHeader>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button className={"bg-[#580848] hover:bg-[#7e1268]"}>Sim</Button>
+                        </DialogClose>
+                        <DialogClose asChild>
+                            <Button className={"bg-[#95727d] hover:bg-[#c0728b]"}>Não</Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+
+            <Dialog open={showShareDialog} onOpenChange={setShowShareDialog}>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Notificações</DialogTitle>
+                    </DialogHeader>
+                    <Card className="mb-4">
+                        <CardHeader className="flex flex-row items-center space-x-4">
+                            <Avatar>
+                                <AvatarImage src={'https://i.pravatar.cc/150?img=10'} alt={'Larissa Gomes'}/>
+                                <AvatarFallback>{'Larissa Gomes'}</AvatarFallback>
+                            </Avatar>
+                            <div>
+                                <p className="font-medium">{'Larissa Gomes compartilhou:'}</p>
+                                <p className="text-sm text-muted-foreground">
+                                    'Fiz meu primeiro commit em um projeto open-source! ❤️'
+                                </p>
+                            </div>
+                        </CardHeader>
+                    </Card>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button className={"bg-[#580848] hover:bg-[#7e1268]"}>Sair</Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+
+            <Dialog open={showMessageDialog} onOpenChange={setShowMessageDialog}>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Mensagens</DialogTitle>
+                    </DialogHeader>
+                    <Card className="mb-4">
+                        <CardHeader className="flex flex-row items-center space-x-4">
+                            <Avatar>
+                                <AvatarImage src={'https://i.pravatar.cc/150?img=8'} alt={'Fernanda Rocha'}/>
+                                <AvatarFallback>{'Fernanda Rocha'}</AvatarFallback>
+                            </Avatar>
+                            <div>
+                                <p className="font-medium">{'Fernanda Rocha:'}</p>
+                                <p className="text-sm text-muted-foreground">
+                                    Eai Matheus! Tudo bem? Queria trocar uma ideia sobre aquele projeto de UI/UX que você mencionou.
+                                </p>
+                            </div>
+                        </CardHeader>
+                    </Card>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button className={"bg-[#580848] hover:bg-[#7e1268]"}>Sair</Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </>
+    )
+}
+
 export const Feed = () => {
     return (
         <div>
@@ -170,9 +288,7 @@ export const Feed = () => {
                         placeholder="Buscar..."
                         className="border border-white rounded-3xl p-2 w-1/2 text-lg bg-white text-black"
                     />
-                    <Link to="/feed">
-                        <Account/>
-                    </Link>
+                    <DropdownMenuDialog />
                 </nav>
             </div>
             <div
@@ -182,7 +298,10 @@ export const Feed = () => {
                         placeholder="O que você está pensando?"
                         className="w-full border border-gray-400 rounded-md text-lg min-h-[100px]"
                     />
-                    <Button className="w-full bg-sky-600 hover:bg-sky-700 text-white px-6 py-2">
+                    <Button
+                        className="w-full bg-[#580848] hover:bg-[#7e1268] text-white px-6 py-2"
+                        onClick={() => alert('Muito obrigado por compartilhar sua ideia de negócio!\nEsperamos que o Shark-in ajude você a encontrar investidores interessados.')}
+                    >
                         Postar
                     </Button>
                 </div>
